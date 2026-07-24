f4c9775348921f1603c9e746350d991511a8873a
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-07-24 13:51:04 +0100

    audio

--- src/main.c
+++ src/main.c
@@ -20,9 +20,11 @@ typedef int32_t i32;
 typedef int16_t i16;
 typedef int8_t i8;
 
+typedef i32 b32;
+
 global_variable i32 WINDOW_WIDTH = 1600;
 global_variable i32 WINDOW_HEIGHT = 900;
-global_variable bool Running = true;
+global_variable b32 Running = true;
 typedef struct {
   void *Memory;
   i32 MemorySize;
@@ -32,9 +34,13 @@ typedef struct {
 } BitmapBuffer;
 global_variable BitmapBuffer GlobalBackBuffer =
     (BitmapBuffer){.BytesPerPixel = 4};
+typedef struct {
+  f32 ToneHz;
+  f32 tSine;
+  i32 SamplesPerSecond;
+} AudioState;
 
-internal void RenderWeirdGradiant(BitmapBuffer Buffer, i32 Xoffset,
-                                  i32 Yoffset) {
+internal void RenderBackBuffer(BitmapBuffer Buffer, i32 Xoffset, i32 Yoffset) {
   int pitch = Buffer.Width * Buffer.BytesPerPixel;
   u8 *row = (u8 *)Buffer.Memory;
 
@@ -50,6 +56,7 @@ internal void RenderWeirdGradiant(BitmapBuffer Buffer, i32 Xoffset,
   }
 }
 
+//------------------------Drawing--------------------------------------------
 internal void AllocateBitmap(BitmapBuffer *Buffer) {
   Buffer->Width = WINDOW_WIDTH;
   Buffer->Height = WINDOW_HEIGHT;
@@ -89,9 +96,25 @@ internal void UpdateWindow(BitmapBuffer Buffer, SDL_Renderer *renderer,
   SDL_RenderPresent(renderer);
 }
 
+//------------------------Audio--------------------------------------------
+void ToggleAudio(SDL_AudioDeviceID AudioDeviceID, b32 *IsAudioPaused) {
+  *IsAudioPaused = !*IsAudioPaused;
+  SDL_PauseAudioDevice(AudioDeviceID, *IsAudioPaused);
+}
+
+void AudioCallback(void *userdata, Uint8 *stream, int len) {
+  i16 *SampleOut = (i16 *)stream;
+  int SampleCount = len / sizeof(i16);
+  for (int i = 0; i < SampleCount; ++i) {
+    SampleOut[i] = (rand() % 6000) - 3000;
+  }
+}
+
+//------------------------Event--------------------------------------------
 internal void HandleEvent(BitmapBuffer *Buffer, SDL_Window *window,
                           SDL_Renderer *Renderer, SDL_Texture **Texture,
-                          SDL_Event event) {
+                          SDL_Event event, SDL_AudioDeviceID AudioDeviceID,
+                          b32 *IsAudioPaused) {
   switch (event.type) {
   case SDL_QUIT: {
     printf("[INFO] user quit\n");
@@ -108,11 +131,33 @@ internal void HandleEvent(BitmapBuffer *Buffer, SDL_Window *window,
       ResizeDIBSection(Buffer, Renderer, Texture);
     }
   } break;
+  case SDL_KEYDOWN: {
+    if (event.key.repeat == 0 &&
+        event.key.keysym.scancode == SDL_SCANCODE_SPACE) {
+      ToggleAudio(AudioDeviceID, IsAudioPaused);
+    }
+  } break;
+  }
+}
+
+//------------------------Movment--------------------------------------------
+void Movement(const u8 *KeyboardState, i32 *x, i32 *y) {
+  if (KeyboardState[SDL_SCANCODE_RIGHT] || KeyboardState[SDL_SCANCODE_D]) {
+    *x += 1;
+  }
+  if (KeyboardState[SDL_SCANCODE_LEFT] || KeyboardState[SDL_SCANCODE_A]) {
+    *x -= 1;
+  }
+  if (KeyboardState[SDL_SCANCODE_UP] || KeyboardState[SDL_SCANCODE_W]) {
+    *y += 1;
+  }
+  if (KeyboardState[SDL_SCANCODE_DOWN] || KeyboardState[SDL_SCANCODE_S]) {
+    *y -= 1;
   }
 }
 
 int main() {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
     fprintf(stderr, "failed to init SDL\n");
     return 1;
   }
@@ -125,34 +170,41 @@ int main() {
                                            SDL_TEXTUREACCESS_STREAMING,
                                            WINDOW_WIDTH, WINDOW_HEIGHT);
 
-  if (Window == NULL || Renderer == NULL) {
+  AudioState AudioState = {.ToneHz = 256.0f, .SamplesPerSecond = 48000};
+  const SDL_AudioSpec AudioDesired = {
+      .freq = 48000,
+      .format = AUDIO_S16LSB,
+      .channels = 2,
+      .samples = 4096,
+      .callback = AudioCallback,
+      .userdata = &AudioState,
+  };
+  SDL_AudioSpec AudioObtained;
+  const char *Device = SDL_GetAudioDeviceName(1, 0);
+  SDL_AudioDeviceID AudioDeviceID = SDL_OpenAudioDevice(
+      Device, 0, &AudioDesired, &AudioObtained, SDL_AUDIO_ALLOW_ANY_CHANGE);
+
+  if (Window == NULL || Renderer == NULL || AudioDeviceID == 0) {
     fprintf(stderr, "[ERR] Failed initilization: %s\n", SDL_GetError());
     return 1;
   }
+
+  const u8 *KeyboardState = SDL_GetKeyboardState(NULL);
+  b32 IsAudioPaused = true;
+
   i32 x = 0;
   i32 y = 0;
 
-  const u8 *KeyboardState = SDL_GetKeyboardState(NULL);
   AllocateBitmap(&GlobalBackBuffer);
   while (Running) {
     SDL_Event Event;
     while (SDL_PollEvent(&Event)) {
-      HandleEvent(&GlobalBackBuffer, Window, Renderer, &Texture, Event);
+      HandleEvent(&GlobalBackBuffer, Window, Renderer, &Texture, Event,
+                  AudioDeviceID, &IsAudioPaused);
     }
-    if (KeyboardState[SDL_SCANCODE_RIGHT]) {
-      x += 1;
-    }
-    if (KeyboardState[SDL_SCANCODE_LEFT]) {
-      x -= 1;
-    }
-    if (KeyboardState[SDL_SCANCODE_UP]) {
-      y += 1;
-    }
-    if (KeyboardState[SDL_SCANCODE_DOWN]) {
-      y -= 1;
-    }
+    Movement(KeyboardState, &x, &y);
     UpdateWindow(GlobalBackBuffer, Renderer, Texture);
-    RenderWeirdGradiant(GlobalBackBuffer, x, y);
+    RenderBackBuffer(GlobalBackBuffer, x, y);
   }
   return 0;
 }
